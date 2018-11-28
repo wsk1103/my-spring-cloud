9432860a67ad77e1e72b58a3820a2869796c08dd
root <1261709167@qq.com>
2018-11-28 17:58:11 +0800

    go

--- config-client/src/main/java/com/wsk/config/client/ConfigClientApplication.java
+++ config-client/src/main/java/com/wsk/config/client/ConfigClientApplication.java
@@ -1,12 +1,25 @@
 package com.wsk.config.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
+
+    @Value("${foo}")
+    private String foo = "wsk";
+
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+
 }
