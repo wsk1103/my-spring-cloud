49f527984fa685c8e693b016dfd64ebf465544c2
root <1261709167@qq.com>
2018-11-30 17:34:21 +0800

    add okhttp

--- common/src/main/java/com/wsk/common/utils/HttpUtil.java
+++ common/src/main/java/com/wsk/common/utils/HttpUtil.java
@@ -1,8 +1,6 @@
 package com.wsk.common.utils;
 
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+import okhttp3.*;
 
 import java.io.IOException;
 
@@ -13,31 +11,57 @@ import java.io.IOException;
  * @date 2018/11/30  16:05
  */
 public class HttpUtil {
-    public static void get(String url) {
-        try {
-            Document document = Jsoup.connect(url).get();
-            System.out.println(document.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+    private static OkHttpClient client = new OkHttpClient();
+
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    /**
+     * get请求
+     *
+     * @param url 请求地址
+     * @return String
+     * @throws IOException IO
+     */
+    public static String get(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        assert response.body() != null;
+        return response.body().string();
     }
 
-    public static String urlToStringPost(String url) throws IOException {
-        Document document;
-        Connection connection = Jsoup.connect(url).ignoreContentType(true).timeout(5000);
-        document = connection.post();
-        return document.getElementsByTag("body").first().text();
+
+    /**
+     * post请求
+     *
+     * @param url  请求地址
+     * @param json 请求参数
+     * @return String
+     * @throws IOException IO
+     */
+    public static String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+        assert response.body() != null;
+        return response.body().string();
     }
 
-    public static void main(String[] args) throws InterruptedException, IOException {
+    public static void main(String[] args) throws IOException {
         int a = 0;
-        while (true) {
+//        while (true) {
             System.out.println(a);
-            urlToStringPost("http://localhost:8762/hi?name=wsk");
+        System.out.println(get("https://www.oschina.net/"));
 //            Thread.sleep(1000);
-            if (a++ >= 100000) {
-                break;
-            }
-        }
+//            if (a++ >= 100000) {
+//                break;
+//            }
+//        }
     }
 }
