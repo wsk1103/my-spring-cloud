21a2e1f31b7484965e01d1ef70a92662ce1edc2c
wsk <1261709167@qq.com>
2019-01-15 14:04:01 +0800

    删除没有的代码

--- ribbon/src/main/java/com/wsk/ribbon/service/HelloService.java
+++ ribbon/src/main/java/com/wsk/ribbon/service/HelloService.java
@@ -14,7 +14,7 @@ import org.springframework.web.client.RestTemplate;
 public class HelloService {
 
     @Autowired
-    RestTemplate restTemplate;
+    private RestTemplate restTemplate;
 
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
