2daef35f356b0344196149b6ed11a39d2410dfe4
wsk <1261709167@qq.com>
2019-01-18 23:04:50 +0800

    增加注释

--- client/src/main/java/com/wsk/client/ClientApplication.java
+++ client/src/main/java/com/wsk/client/ClientApplication.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author wsk1103
+ */
 @SpringBootApplication
 @EnableEurekaClient
 //@EnableDiscoveryClient
